b26fcfd6860285d4f9d6f277d5f9e0dcc19d6c9e
miyu2000 <tanmaidmuke@gmail.com>
2020-12-21 15:56:52 +0530

    Latest One copy2 !!

---
+++ lib/restaurant_class.dart
@@ -0,0 +1,30 @@
+class restaurant_data {
+  //String user_id;
+  final String type1;
+  final String item1;
+  final String type2;
+  final String item2;
+  /*final String address;
+  final String location;*/
+
+  restaurant_data(this.type1,this.item1, this.type2, this.item2);
+
+  /*User.fromJson(Map<String, dynamic> json)
+      : email = json['email'],
+        password = json['password'];
+        //contact=json['contact_number'];*/
+
+
+
+
+
+  Map<String, dynamic> toJson() =>
+      {
+        'type1': type1,
+        'item1': item1,
+        'type2': type2,
+        'item2': item2
+      };
+}
+
+
